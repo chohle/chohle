fecf612f67cb8061c6f44284164b36d29f61a88d
zeaiso <emanuell.ademi@hotmail.com>
2026-06-03 21:30:57 +0200

    chore: adjust favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,10 +29,10 @@ export default defineNuxtConfig({
       title: 'chohle',
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/chohle.svg' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
-        { rel: 'shortcut icon', href: '/favicon/favicon.ico' }
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
+        { rel: 'shortcut icon', href: '/favicons/favicon.ico' }
       ]
     }
   },
